9cd439eaae10aa58aa9993621be58b7de4f96d2c
Learning-howto-Code <jaketorohopkins@icloud.com>
2026-07-06 15:47:05 -0700

    updated pcb, added platformio workspace

---
+++ code/servo test/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+
+Servo myServo;
+
+void setup() {
+  myServo.attach(9);  // signal wire on pin 9
+}
+
+void loop() {
+  myServo.write(0);
+  delay(1000);
+  myServo.write(90);
+  delay(1000);
+  myServo.write(180);
+  delay(1000);
+}
